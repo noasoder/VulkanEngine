3214eedb52daa607533727fd64ba5344e9b50b4a
Noa Söderlund <noasoderlund01@gmail.com>
2021-08-22 23:39:41 +0200

    libraries

--- Premake/premake5.lua
+++ Premake/premake5.lua
@@ -8,8 +8,8 @@ workspace "VulkanEngine"
         architecture "x86_64"
         cppdialect "C++17"
 
-    includedirs { "C:/VulkanSDK/1.2.182.0/Include", "C:/Libraries/glfw-3.3.4.bin.WIN64/include", "C:/Libraries/glm" }
-    libdirs { "C:/VulkanSDK/1.2.182.0/Lib", "C:/Libraries/glfw-3.3.4.bin.WIN64/lib-vc2019" }
+    includedirs { "C:/VulkanSDK/1.2.182.0/Include", "../Libraries/glfw-3.3.4.bin.WIN64/include", "../Libraries/glm", "../Libraries/stb" }
+    libdirs { "C:/VulkanSDK/1.2.182.0/Lib", "../Libraries/glfw-3.3.4.bin.WIN64/lib-vc2019" }
     links { "vulkan-1.lib", "glfw3.lib" }
 
 project "VulkanEngine"
